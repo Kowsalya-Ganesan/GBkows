7de5760fe41f8ee6d2ba5780468ea9589b7cb805
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-20 20:11:38 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,20 @@
+#code
+
+val=1
+n=int(input())
+c=int(input())
+v=2*c
+for i in range (0,n):
+    if(i%2==0):
+        for j in range (0,c):
+            print(val,end=" ")
+            
+            val=val+1
+        val=val+c
+        print("\n")
+    else:
+        for j in range (0,c):
+            print(v,end=" ")
+            v=v-1 
+        v=(v+1)+((2*c)+(c-1))
+        print("\n")
