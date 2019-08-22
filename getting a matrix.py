e3c64315305a65e00af17b696fee150f9bf0c567
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-22 08:24:27 +0530

    Add files via upload

---
+++ getting a matrix.py
@@ -0,0 +1,18 @@
+#code
+'''n=int(input())
+arr=[]
+for _ in range(n):
+
+    x=int(input())
+    arr.append(x)
+print(arr)'''
+
+'''x=[int(i) for i in input().split()]
+print(x)'''
+
+'''rr=[int(input()) for i in range(n)]
+print(arr)'''
+
+x=list(map(int,input().split()))
+print(x)
+
