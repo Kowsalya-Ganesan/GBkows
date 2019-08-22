a7435cfebd6988ef0fb44003c2673b3e3018b933
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-22 08:36:46 +0530

    Add files via upload

---
+++ find student with max avg.py
@@ -0,0 +1,27 @@
+﻿#code
+n=int(input())
+lst=[]
+ne=[]
+for i in range(0,n):
+    d=input().split()
+    lst.append(d)
+    k=lst[i]
+    avg=sum(map(float,k[1:]))/3
+    ne.append(avg)
+print(ne)
+t=max(ne)
+print(t)
+for i in range (0,len(ne)):
+    if(ne[i]==t):
+        ind=i
+print(ind)
+print(lst[ind][0])
+
+
+
+
+
+
+
+   
+
