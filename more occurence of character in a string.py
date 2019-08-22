68ddc10e12007fa9c75c6ef5ae4c44b98923424e
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-22 08:19:47 +0530

    Add files via upload

---
+++ more occurence of character in a string.py
@@ -0,0 +1,18 @@
+#code
+st=input()
+lst=[]
+for i in range (0,len(st)):
+    if(st[i].isalpha()):
+        lst.append(st[i])
+for i in range (0,len(lst)):
+    for j in range (i+1,len(st)):
+        if(st[i]==st[j]):
+            print(st[i])
+         
+         
+input:
+    abnqdan
+output:
+    a
+    n
+  
