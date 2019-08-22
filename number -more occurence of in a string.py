b5e369c4777c967ebade0424d00821c74d2490f1
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-22 07:54:24 +0530

    Add files via upload

---
+++ number -more occurence of in a string.py
@@ -0,0 +1,22 @@
+﻿import sys
+
+if __name__ == '__main__':
+    st=raw_input()
+    lst=[]
+    ma=0
+    for i in st:
+        if i.isdigit():
+            lst.append(i)
+    print(lst)
+    s=str(lst)
+    for i in lst:
+        c=lst.count(i)
+        if c>ma:
+            ma=c
+            k=i
+    print k
+            
+        
+        
+     
+
