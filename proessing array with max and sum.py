a258d212c1d0aceb6394bbd2fcdabc06576a0e0a
Kowsalya-Ganesan <54320501+Kowsalya-Ganesan@users.noreply.github.com>
2019-08-22 08:27:31 +0530

    Add files via upload

---
+++ proessing array with max and sum.py
@@ -0,0 +1,54 @@
+#code
+n=int(input())
+lst=list(map(int,input().split()))
+sum=0
+l=max(lst)
+while(l):
+    r=l%10
+    sum=sum+r
+    l=l//10
+for i in range(0,len(lst)):
+    k=lst[i]/sum
+    print("%.2f"%k,end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+       
+ 
+   
+
+
+
+
+
+
+   
